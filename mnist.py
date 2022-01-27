72e6ea9b538baf6583f9804a28835d3af556e9f8
eitanpushett <95567545+eitanpushett@users.noreply.github.com>
2022-01-27 12:01:42 +0200

    fixed

--- mnist.py
+++ mnist.py
@@ -1,37 +1,65 @@
 '''Trains a simple convnet on the MNIST dataset.
+
 Gets to 99.25% test accuracy after 12 epochs
 (there is still a lot of margin for parameter tuning).
 16 seconds per epoch on a GRID K520 GPU.
 '''
 
-
-from __future__ import print_function
 import keras
-import tensorflow as tf
-#from keras import utils as np_utils
+import tensorflow.keras as keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+import tensorflow as tf
 import argparse
-import os
 
-if not os.path.exists(os.path.dirname("output/")):
-    os.makedirs(os.path.dirname("output/"))
-
-parser = argparse.ArgumentParser(description='Running MNIST Algorithm')
+parser = argparse.ArgumentParser(description='IMDB model training')
 parser.add_argument('--epochs', help='number of epochs to run', default='12')
 parser.add_argument('--batch_size', help='iteration batch size', default='128')
 args = parser.parse_args()
 
-num_classes = 10
 batch_size = int(args.batch_size)
 epochs = int(args.epochs)
+def save_frozen_pb(model, mod_path):
+    # Convert Keras model to ConcreteFunction
+    full_model = tf.function(lambda x: model(x))
+    concrete_function = full_model.get_concrete_function(
+        x=tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
 
-print('cnvrg_tag_batch_size:', batch_size)
-print('cnvrg_tag_num_classes:', num_classes)
-print('cnvrg_tag_epochs:', epochs)
+    # Get frozen ConcreteFunction
+    frozen_model = convert_variables_to_constants_v2(concrete_function)
+
+    # Generate frozen pb
+    tf.io.write_graph(graph_or_graph_def=frozen_model.graph,
+                      logdir=".",
+                      name=mod_path,
+                      as_text=False)
+
+fp32_frozen_pb_file = "fp32_frozen.pb"
+
+#import numpy as np
+
+class LossAndErrorPrintingCallback(keras.callbacks.Callback):
+    def on_train_batch_end(self, batch, logs=None):
+        print("cnvrg_linechart_{} value: {}".format('loss_train', logs["loss"]))
+        print("For batch {}, loss is {:7.2f}.".format(batch, logs["loss"]))
+
+    def on_test_batch_end(self, batch, logs=None):
+        print("cnvrg_linechart_{} value: {}".format('loss_test', logs["loss"]))
+        print("For batch {}, loss is {:7.2f}.".format(batch, logs["loss"]))
+
+    def on_epoch_end(self, epoch, logs=None):
+        print(
+            "The average loss for epoch {} is {:7.2f} ".format(
+                epoch, logs["loss"])
+        )
+
+
+num_classes = 10
+
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -39,29 +67,21 @@ img_rows, img_cols = 28, 28
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print('cnvrg_tag_x_train_shape:', x_train.shape)
-print('cnvrg_tag_train_samples:', x_train.shape[0])
-print('cnvrg_tag_test_samples:', x_test.shape[0])
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
-
-
-tbCallback = keras.callbacks.TensorBoard(histogram_freq=0, write_graph=True, write_images=True)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -75,18 +95,18 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss= keras.losses.categorical_crossentropy,
-              optimizer= tf.keras.optimizers.Adadelta(),
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          verbose=2,
+          verbose=1,
           validation_data=(x_test, y_test),
-          callbacks=[tbCallback])
-
+          callbacks=[LossAndErrorPrintingCallback()])
 score = model.evaluate(x_test, y_test, verbose=0)
-print('cnvrg_tag_test_loss:', score[0])
-print('cnvrg_tag_test_accuracy:', score[1])
-model.save('output/mnist_model.h5')
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+save_frozen_pb(model, fp32_frozen_pb_file)
+model.save('mnist_model.h5')
